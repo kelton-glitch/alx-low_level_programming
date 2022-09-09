03120ee549f9f63516a6758ffae6b125d41e50eb
Kamgo N Elton <kamgoelton89@gmail.com>
2022-09-09 16:07:50 -0700

    additional

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+  int main(void)
+	{
+		int i;
+		int j;
+
+		for (i = 48; i < 58; j++)
+		{
+			for (j =48; j < 58; j++)
+			{
+				if (i != j && i < j)
+				{
+					putchar(i);
+					putchar(j);
+					if (j == 57 && i == 56)
+					{
+						break;
+				
+					}
+				putchar(',');
+				putchar(' ');
+			
+				}
+		
+			}
+
+		}
+		putchar('\n');
+		return(0);
+	
+	}	
