0bd8d59e11250a417eac221486b469d78e506a05
Kamgo N Elton <kamgoelton89@gmail.com>
2022-09-13 12:44:49 -0700

    fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - Entry point
@@ -6,7 +6,8 @@
  * Return: 0
  */
 
-	int main(void){
+	int main(void)
+	{
 		printf("_putchar\n");
 
 		return (0);
