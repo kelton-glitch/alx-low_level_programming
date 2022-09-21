68c2551aff8b005d3d9bf1a34e5198e606c11afc
Kamgo N Elton <kamgoelton89@gmail.com>
2022-09-21 15:30:33 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string
+ * @dest: resulting value
+ * @src: source string
+ * @n: precises the number of bytes to be used
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
