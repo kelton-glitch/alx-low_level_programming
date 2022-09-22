481775438e960326576b5aa27e6ac11d37137222
Kamgo N Elton <kamgoelton89@gmail.com>
2022-09-22 13:03:58 -0700

    capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string: capitalizes all words of a string
+ * cap_string - capitalizes all words of a string
  * @str: parameter
  * Return: char
  */
@@ -11,18 +11,18 @@ char *cap_string(char *str)
 	int i, count = 0;
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
+	if (*(str + count) >= 97 && *(str + count) <= 122)
+		*(str + count) = *(str + count) - 32;
 	count++;
 
-	while (*(s + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == separators[i])
+			if (*(str + count) == separators[i])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+					*(str + (count + 1)) = *(str + (count + 1)) - 32;
 				break;
 			}
 		}
@@ -30,5 +30,5 @@ char *cap_string(char *str)
 		count++;
 	}
 
-	return (s);
+	return (str);
 }
