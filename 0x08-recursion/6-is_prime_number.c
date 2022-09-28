5bea947faface61ff50b9d72fef202b71637305a
Kamgo N Elton <kamgoelton89@gmail.com>
2022-09-28 15:36:07 -0700

    checks for a prime number and prints 1 if true and 0 otherwise

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * evaluate_num - loop
+ * @n: integer
+ * @iterator: number to iterate
+ * Return: 1 or 0
+ */
+
+int evaluate_num(int n, int iterator)
+{
+	if (iterator == n - 1)
+		return (1);
+
+	else if (n % iterator == 0)
+		return (0);
+
+	if (n % iterator != 0)
+		return (evaluate_num(n, iterator + 1));
+
+	return (0);
+}
+
+
+/**
+ * is_prime_number - checks if a number is prime or not
+ * @n: integer
+ * Return: 1 if number is prime and 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int iterator;
+
+	iterator = 2;
+
+	if (num < 2)
+		return (0);
+
+	if (num == 2)
+		return (1);
+
+	return (evaluate_num(n, iterator));
+}
