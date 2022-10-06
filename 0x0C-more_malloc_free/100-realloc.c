01c0578ffb7f7c480ca41b14d8f69a8000d3a6ad
Kamgo N Elton <kamgoelton89@gmail.com>
2022-10-06 15:30:15 -0700

    fixes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  * Return: pointer
  */
 
-void *_realloc(void *ptr, unsigned int old_size, unsined int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
