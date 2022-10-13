33bd1bf31dcbe40b69b65be9d66732e8529dee46
Kamgo N Elton <kamgoelton89@gmail.com>
2022-10-12 17:22:37 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation demanded
+ * @s: operator given by user
+ *
+ * Return: pointer to a function that corresponds to the pointed operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+
+		i++;
+	}
+
+	return (NULL);
+}
