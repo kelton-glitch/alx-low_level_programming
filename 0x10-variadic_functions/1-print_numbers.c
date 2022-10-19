d05357c08a022fb015a341cd6bc5a22691bac7c0
Elton Kamgo Njomo <elton@panus.cm>
2022-10-19 11:18:07 +0100

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,28 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print num
- * @separator: pointer param
- * @n: param
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
 	unsigned int i;
+	va_list list;
 
-	va_start(valist, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (seperator != NULL && i < n - 1)
-			printf("%s", separator);
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
-	printf("\n");
-	va_end(valist);
-}
+	va_end(list);
 
+	printf("\n");
+}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,33 +1,33 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints string
- * @separator: pointer parameter
- * @n: param
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
 	unsigned int i;
 	char *str;
 
-	va_start(valist, n);
+	va_list list;
+
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(valist, char *);
-
-		if (str)
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
 			printf("%s", str);
 		else
-			printf("(nil)");
-
-		if (i < n - 1)
-			if (seperator)
-				printf("%s", separator);
+			printf("%s%s", separator, str);
 	}
 
 	printf("\n");
-	va_end(valist);
+
+	va_end(list);
 }
