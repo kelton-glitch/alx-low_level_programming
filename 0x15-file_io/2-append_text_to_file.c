d264d78aaa990859b54c98dddf1e426425f4aa0c
Kamgo N Elton <kamgoelton89@gmail.com>
2022-10-31 14:56:41 -0700

    fixes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,36 +5,36 @@
  * @filename: filename
  * @text_content: added content
  *
- * Return : 1 if the file exists. -1 if the file does not exist
+ * Return: 1 if the file exists. -1 if the file does not exist
  * or fails
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nletters;
-    int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (!filename)
-        return (-1);
-    
-    fd = open(filename, O_WRONLY | O_APPEND);
+	if (!filename)
+		return (-1);
 
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (text_content)
-    {
-        for (nletters = 0; text_content[nletters]; nletters++)
-            ;
-        
-        rwr = write(fd, text_content, nletters);
+	if (fd == -1)
+		return (-1);
 
-        if (rwr == -1)
-            return (-1);
-    }
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-    close(fd);
+		rwr = write(fd, text_content, nletters);
 
-    return (1);
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
