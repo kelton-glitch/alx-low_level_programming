5505ed9d684a59ffc11e4a3d3d35876c2282646b
Kamgo N Elton <kamgoelton89@gmail.com>
2023-04-11 13:51:33 +0100

    linear skip

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -22,6 +22,24 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ * 
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 /* Function that searches for a value in a sorted array of integers 
  * using binary search algorithm
  */
@@ -56,6 +74,11 @@ int exponential_search(int *array, size_t size, int value);
  * using the jump search algorithm in a singly linked list
  */
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/* Function that searches for a value in a sorted array of integers
+ * using the linear search algorithm in a skip list
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 /* print_list - Prints the content of a listint_t*/
 void print_list(const listint_t *list);
