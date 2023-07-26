5bed021986a0ce1fbb35849a1190fc041c98cfe5
Kamgo N. Elton <kamgoelton89@gmail.com>
2023-07-26 14:00:41 +0000

    search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Functionn that sarches for a value in an
+ * array of integers using linear search algorithm
+ *
+ * @array: Array is a pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search in the array
+ *
+ * Return: The first index of where value is located
+ * or -1 if the value is no found or if array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,5 @@
+# Search Algorithms
+
+Working on search algorithms based on the different search principles
+found in the principles of the C programming langugage
+
